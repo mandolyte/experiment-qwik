ff0f17275f2d3ae08ecb7944751b2ab3d30befcf
mandolyte <cecil.new@gmail.com>
2023-01-15 08:36:52 -0500

    cleanup

--- renderDjot/src/routes/index.tsx
+++ renderDjot/src/routes/index.tsx
@@ -36,20 +36,35 @@ export default component$(() => {
   );
 });
 
+/* here is a sample Djot text for a bit of scripture to test with.
+   the styling attributes were added to the global.css file.
 
-/* This from a hint in discord
+JUDE{.book}
 
-import { component$, useSignal } from '@builder.io/qwik';
+Greeting{.heading}
 
-export default component$(() => {
-  const previewRef = useSignal<HTMLDivElement>();
-  return <div>
-    <textarea onInput$={(e) => {
-      if (previewRef.value) {
-        previewRef.value.innerHTML = (e.target as HTMLTextAreaElement).value
-      }
-    }} />
-    <div ref={previewRef} />
-  </div>;
-});
+1{.verse} 
+Jude, a servant[^1]{.footnote} of Jesus Christ and brother of James,
+[^a]{.reference}To those who are called, 
+[^b]{.reference}beloved in God the Father and
+[^c]{.reference}kept
+for[^2]{.footnote} Jesus Christ:
+
+2{.verse} May 
+[^d]{.reference}mercy, 
+[^e]{.reference}peace, and love be multiplied to you.
+
+[^1]: For the contextual rendering of the Greek word _doulos_ see Preface.
+
+[^a]: Romans 1:7; 1 Corinthians 1:24
+
+[^b]: 1 Thessalonians 1:4; 2 Thessalonians 2:13
+
+[^c]: John 17:11; John 17:15; 1 Thessalonians 5:23
+
+[^2]: Or _by_
+
+[^d]: 2 John 3
+
+[^e]: 1 Peter 1:2; 2 Peter 1:2
 */
