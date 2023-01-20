87173410c0d71572f4e72b4764abfb95b0cec097
mandolyte <cecil.new@gmail.com>
2023-01-20 09:58:38 -0500

    changed import style

--- mui/src/integrations/react/mui.tsx
+++ mui/src/integrations/react/mui.tsx
@@ -3,5 +3,5 @@
 import { qwikify$ } from '@builder.io/qwik-react';
 // import { Button, Slider } from '@mui/material';
 // import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
-import { Button } from '@mui/material';
+import Button from '@mui/material/Button';
 export const MUIButton = qwikify$(Button);
