a498bf79ecda50f270f4ef6eecd27a17e14d776a
mandolyte <cecil.new@gmail.com>
2023-01-28 17:37:14 -0500

    added a function to the state using useSignal

--- context/src/routes/index.tsx
+++ context/src/routes/index.tsx
@@ -2,6 +2,9 @@ import {
   component$,
   useStore,
   useContextProvider,
+  useClientEffect$,
+  noSerialize,
+  useSignal,
 } from '@builder.io/qwik';
 
 import type {stateIF} from '../components/common';
@@ -9,11 +12,20 @@ import MyContext from '../components/common';
 
 import { Child } from './child';
 
+export function logger(arg:number): void {
+  console.log("Value of count is:",arg)
+}
 
 export const Parent = component$(() => {
   // Create some reactive storage
   const state = useStore<stateIF>({
     count: 0,
+  });
+
+  const fstate = useSignal<typeof logger|null>(null)
+
+  useClientEffect$(() => {
+    fstate.value = noSerialize(logger);
   });
 
   // Assign value (state) to the context (MyContext)
@@ -22,6 +34,7 @@ export const Parent = component$(() => {
     <>
       <Child />
       <div>Count: {state.count}</div>
+      <div><button onClick$={() => fstate.value(state.count)}>Log value to console</button></div>
     </>
   );
 });
