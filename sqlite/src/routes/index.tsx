bc6337071476e1a29eb3d5007399a4c123350fca
mandolyte <cecil.new@gmail.com>
2023-02-18 16:56:06 -0500

    added jsasm folder

--- sqlite/src/routes/index.tsx
+++ sqlite/src/routes/index.tsx
@@ -1,6 +1,21 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useBrowserVisibleTask$ } from '@builder.io/qwik';
+import { sqlite3InitModule } from './jswasm/sqlite3'
 
 export default component$(() => {
+  sqlite3InitModule && sqlite3InitModule().then(function(sqlite3: any){
+    // The module is now loaded and the sqlite3 namespace
+    // object was passed to this function.
+    console.log("sqlite3:", sqlite3);
+  });
+
+  useBrowserVisibleTask$(() => {
+    sqlite3InitModule && sqlite3InitModule().then(function(sqlite3: any){
+      // The module is now loaded and the sqlite3 namespace
+      // object was passed to this function.
+      console.log("sqlite3:", sqlite3);
+    });
+  });
+
   return (
     <div>
       <h1>Start of the Sqlite Experiment</h1>
