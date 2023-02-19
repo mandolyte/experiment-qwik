c426a391e86ade29eb1f22348043ad6eee6afd5a
mandolyte <cecil.new@gmail.com>
2023-02-18 20:21:30 -0500

    ckpt

--- sqlite/src/routes/index.tsx
+++ sqlite/src/routes/index.tsx
@@ -2,14 +2,14 @@ import { component$, useBrowserVisibleTask$ } from '@builder.io/qwik';
 import { sqlite3InitModule } from './jswasm/sqlite3'
 
 export default component$(() => {
-  sqlite3InitModule && sqlite3InitModule().then(function(sqlite3: any){
-    // The module is now loaded and the sqlite3 namespace
-    // object was passed to this function.
-    console.log("sqlite3:", sqlite3);
-  });
+  // sqlite3InitModule && sqlite3InitModule().then(function(sqlite3: any){
+  //   // The module is now loaded and the sqlite3 namespace
+  //   // object was passed to this function.
+  //   console.log("sqlite3:", sqlite3);
+  // });
 
   useBrowserVisibleTask$(() => {
-    sqlite3InitModule && sqlite3InitModule().then(function(sqlite3: any){
+    sqlite3InitModule &&sqlite3InitModule().then(function(sqlite3: any){
       // The module is now loaded and the sqlite3 namespace
       // object was passed to this function.
       console.log("sqlite3:", sqlite3);
